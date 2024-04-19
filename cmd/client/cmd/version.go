0bc565f5cd7056a761c9f8bf0a11f0b8a9a9ec6d
Christian Ege <ch@ege.io>
2024-04-19 12:26:27 +0200

    fix: the version command of the norn-client

--- cmd/client/cmd/version.go
+++ cmd/client/cmd/version.go
@@ -16,47 +16,11 @@ limitations under the License.
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 
-	"github.com/nornforge/norn/pkg/norn"
 	"github.com/nornforge/norn/version"
 	"github.com/spf13/cobra"
-	"go.bug.st/serial"
 )
-
-func getDeviceVersion(portName string) (string, error) {
-	command := norn.Command{Type: norn.Version}
-	mode := &serial.Mode{
-		BaudRate: 115200,
-		Parity:   serial.NoParity,
-		DataBits: 8,
-		StopBits: serial.OneStopBit,
-	}
-	port, err := serial.Open(portName, mode)
-	if err != nil {
-		return "", fmt.Errorf(
-			"unable to open the serial port: %s",
-			portName,
-		)
-	}
-	defer port.Close()
-	port.Write(command.Marshal())
-	reader := bufio.NewReader(port)
-	response := norn.Response{}
-	err = response.Parse(reader)
-	if err != nil {
-		return "", err
-	}
-
-	if !response.Success {
-		return "", fmt.Errorf("unable to get the version info from the device: %w", err)
-	}
-
-	return response.Message, nil
-}
-
-// ...
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -65,19 +29,8 @@ var versionCmd = &cobra.Command{
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		portName, err := rootCmd.PersistentFlags().GetString("device")
-		if err != nil {
-			return err
-		}
-
 		version := version.GetProgramVersion()
 		fmt.Printf("Program Version : v%s\n", version.String())
-		deviceVersion, err := getDeviceVersion(portName)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("Device Version  : %s\n", deviceVersion)
-
 		return nil
 	},
 }
